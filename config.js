6a4b3f94d84968a2bbe32f7a60b65c1c39d4cb2d
mojixiang <moji_xiang@163.com>
2014-08-21 18:41:12 +0800

    push project

---
+++ config.js
@@ -0,0 +1,11 @@
+var pkg = require('./package.json');
+var config = {
+	name : "澳江紧固件制造有限公司",
+	description : "专业制造T型钉",
+	version : pkg.version,
+	username : 'aojiang',
+	password : 'qiqiuling'
+};
+
+module.exports.config = config;
+
